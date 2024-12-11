ee9aad4b670f8fdb835ee4d5cf12b0e3904f3d9f
Scarletbobcat <tien.hoan314@yahoo.com>
2024-12-11 14:16:34 -0500

    Remove to-do list from home page

--- client/src/Home/Home.tsx
+++ client/src/Home/Home.tsx
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div>
-        <p>To Do:</p>
-        <ul>
-          <li>Add things to home page - looks pretty empty</li>
-        </ul>
-      </div>
+      <div></div>
     </>
   );
 }
