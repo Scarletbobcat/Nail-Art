df48d6d30021a117d8595fce207101ac308f65fe
Scarletbobcat <tien.hoan314@yahoo.com>
2025-02-03 16:32:51 -0500

    Updated cron job to also remove appointments from archived appointments collection that are older than one month

--- cron/ArchiveAppointments.py
+++ cron/ArchiveAppointments.py
@@ -12,14 +12,17 @@ client = pymongo.MongoClient(connection_string)
 db = client["Nail-Art"]
 
 appointments = db["Appointments"]
-# db.create_collection("ArchivedAppointments")
 archived_appointments = db["ArchivedAppointments"]
 
 today = datetime.date.today()
 
-past_appointments = appointments.find({"date": {"$lt": str(today)}})
+past_appointments = appointments.find({"date": {"$lt": str(today)}}).to_list()
 
-if (past_appointments.to_list()):
+month_ago = today - datetime.timedelta(days=30)
+
+appoinments_greater_than_month = archived_appointments.find({"date": {"$lt": str(month_ago)}}).to_list()
+
+if (past_appointments):
   print("Archiving appointments...")
   # inserting into archive collection
   archived_appointments.insert_many(past_appointments)
@@ -28,3 +31,10 @@ if (past_appointments.to_list()):
   print("Archived appointments successfully!")
 else:
   print("No appointments to archive.")
+
+if (appoinments_greater_than_month):
+  print("Removing appointments older than a month...")
+  archived_appointments.delete_many({"date": {"$lt": str(month_ago)}})
+  print("Removed appointments older than a month successfully!")
+else:
+  print("No archived appointments to remove.")
