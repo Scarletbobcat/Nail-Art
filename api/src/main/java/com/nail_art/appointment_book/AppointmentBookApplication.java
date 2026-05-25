96322d031d0d17a3247f8fbd411fe249e59a985c
Scarletbobcat <tien.hoang314@yahoo.com>
2026-05-25 12:47:36 -0400

    chore: decouple boot from mongo autoconfig

--- api/src/main/java/com/nail_art/appointment_book/AppointmentBookApplication.java
+++ api/src/main/java/com/nail_art/appointment_book/AppointmentBookApplication.java
@@ -1,10 +1,15 @@
 package com.nail_art.appointment_book;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		MongoAutoConfiguration.class,
+		MongoDataAutoConfiguration.class
+})
 @EnableScheduling
 public class AppointmentBookApplication {
 
--- api/src/main/java/com/nail_art/appointment_book/configs/MongoConfig.java
+++ api/src/main/java/com/nail_art/appointment_book/configs/MongoConfig.java
@@ -1,43 +1,38 @@
 package com.nail_art.appointment_book.configs;
 
 import com.mongodb.client.MongoClient;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import org.bson.Document;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mongodb.client.MongoClients;
+import com.mongodb.ConnectionString;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.index.Index;
-import org.springframework.data.mongodb.core.index.PartialIndexFilter;
 
 @Configuration
 @ConditionalOnProperty(name = "app.mongo-config.enabled", havingValue = "true", matchIfMissing = true)
 public class MongoConfig {
-    @Autowired
-    private MongoClient mongoClient;
+    private static final String DEFAULT_DATABASE = "nail_art";
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
+    @Value("${app.mongodb.uri:mongodb://localhost:27017/" + DEFAULT_DATABASE + "}")
+    private String mongoUri;
 
-    @PostConstruct
-    public void ensureIndexes() {
-        mongoTemplate.indexOps("Clients").ensureIndex(
-                new Index()
-                        .on("phoneNumber", Sort.Direction.ASC)
-                        .unique()
-                        .named("phoneNumber_unique_partial")
-                        .partial(PartialIndexFilter.of(
-                                Document.parse("{ phoneNumber: { $type: 'string', $gt: '' } }")
-                        ))
-        );
+    @Bean(destroyMethod = "close")
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
     }
 
-    @PreDestroy
-    public void closeMongoClient() {
-        if (mongoClient != null) {
-            mongoClient.close();
-        }
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        String database = connectionString.getDatabase() == null ? DEFAULT_DATABASE : connectionString.getDatabase();
+        return new SimpleMongoClientDatabaseFactory(mongoClient, database);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTemplate(mongoDatabaseFactory);
     }
 }
